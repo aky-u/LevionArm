5dce1be8e16ff4c4483b601eb549091cc0492818
akiyoshi <uchida.akiyoshi.s3@dc.tohoku.ac.jp>
2025-03-27 19:45:52 +0100

    Implement CAN communication test in test_can.cpp and remove unused header file

--- test/test_motor_connection/include/test_motor_connection/test_can.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef LEVIONARM__TEST_MOTOR_CONNECTION__TEST_CAN_HPP_
-#define LEVIONARM__TEST_MOTOR_CONNECTION__TEST_CAN_HPP_
-
-namespace levionarm::test_motor_connection
-{
-} // namespace levionarm::test_motor_connection
-
-#endif // LEVIONARM__TEST_MOTOR_CONNECTION__TEST_CAN_HPP_
